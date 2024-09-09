44b103574a07471d30c0608c537e727c4a2fa54b
Michael <mlynch@networkanswer.com.au>
2024-09-09 10:05:07 +1000

    .net Thread and list tester

---
+++ WinGDITesterFramework/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WinGDSTesterFramework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ResultsList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelBackgroundCS = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 446);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add to list";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(239, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Locking test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(240, 209);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(74, 20);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(320, 211);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(128, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Mutexs (potential threads)";
+            // 
+            // ResultsList
+            // 
+            this.ResultsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.ResultsList.HideSelection = false;
+            this.ResultsList.Location = new System.Drawing.Point(240, 288);
+            this.ResultsList.Name = "ResultsList";
+            this.ResultsList.Size = new System.Drawing.Size(548, 135);
+            this.ResultsList.TabIndex = 14;
+            this.ResultsList.UseCompatibleStateImageBehavior = false;
+            this.ResultsList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Lock iters";
+            this.columnHeader1.Width = 84;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Global CS/s";
+            this.columnHeader2.Width = 90;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Iter per ms";
+            this.columnHeader3.Width = 75;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "CPU Cycles per iter";
+            this.columnHeader4.Width = 112;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "CS per iter";
+            this.columnHeader5.Width = 83;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Core usage";
+            this.columnHeader6.Width = 72;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(240, 261);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "1 iter = WaitOne+Release";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(321, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "CPU0";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(402, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Any CPU";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // labelBackgroundCS
+            // 
+            this.labelBackgroundCS.AutoSize = true;
+            this.labelBackgroundCS.Location = new System.Drawing.Point(243, 190);
+            this.labelBackgroundCS.Name = "labelBackgroundCS";
+            this.labelBackgroundCS.Size = new System.Drawing.Size(92, 13);
+            this.labelBackgroundCS.TabIndex = 18;
+            this.labelBackgroundCS.Text = "Background CS/s";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(219, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Note that Global CS/s is a system wide figure";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(483, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "First 4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(564, 235);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Last 4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelBackgroundCS);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ResultsList);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListView ResultsList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labelBackgroundCS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WinGDITesterFramework/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinGDSTesterFramework
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter backgroundCS;
+        private PerformanceCounter switchCounter;
+        private Stopwatch stopwatch;
+        private Process us;
+        private long lastCpuTimeSample;
+        private float lastGCSSample;
+        private long lastTimeSample;
+        private ulong lastCyclesSample;
+        private ManagementObject usPerf;
+
+        //private IntPtr originalAffinity;
+
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool QueryProcessCycleTime(IntPtr ProcessHandle, out ulong CycleTime);
+        public Form1()
+        {
+            InitializeComponent();
+            Process.GetCurrentProcess().Refresh();
+            //Doesnt work, just gives 15, which is the first 4 cores.
+            //originalAffinity = Process.GetCurrentProcess().ProcessorAffinity;
+            backgroundCS = new PerformanceCounter("System", "Context Switches/sec", "");
+            backgroundCS.NextSample();
+            switchCounter = new PerformanceCounter("System", "Context Switches/sec", "");//slow call.
+            stopwatch = new Stopwatch();
+            us = Process.GetCurrentProcess();
+            var mos = new ManagementObjectSearcher("SELECT * FROM Win32_Process where Handle = " + us.Id);
+            foreach (ManagementObject obj in mos.Get()) { usPerf = obj; break; }
+            //usPerf.Get();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var start = DateTime.Now;
+            var rnd = new Random();
+            for (var i = 0; i < 15000; i++)
+            {
+                listBox1.Items.Add(rnd.NextDouble().ToString());
+            }
+
+            button1.Text = (DateTime.Now - start).ToString();
+        }
+
+        
+        private void SwitchTest(int iters, int cores)
+        {
+            var m = cores;
+            var t = m*2;
+            var muts = new List<Mutex>();
+            var ts = new List<Thread>();
+            for (var mi = 0; mi < m; mi++)
+                muts.Add(new Mutex());
+            for(var i =  0; i < t; i++)
+            {
+                var nt = new Thread((param) =>
+                {
+                    var tid = (int)param;
+                    var mut = muts[tid % m];
+                    for (var j = 0; j < iters; j++)
+                    {
+                        mut.WaitOne();
+                        mut.ReleaseMutex();
+                    }
+                });
+                nt.Priority = ThreadPriority.AboveNormal;
+                ts.Add(nt);
+            }
+
+            ulong beginCycles, endCycles;
+
+            //these values arent refreshing.
+            var mos = new ManagementObjectSearcher("SELECT * FROM Win32_Process where Handle = " + us.Id);
+            foreach (ManagementObject obj in mos.Get()) { usPerf = obj; break; }
+            var beginCpuTime = (ulong)usPerf.Properties["KernelModeTime"].Value + (ulong)usPerf.Properties["UserModeTime"].Value;
+            stopwatch.Restart();
+            switchCounter.NextValue();
+            QueryProcessCycleTime(us.Handle, out beginCycles);
+            for (var i = 0; i < t; i++)
+                ts[i].Start(i);
+            for (var i = 0; i < t; i++)
+                ts[i].Join();
+
+            //Dont do this under the debugger.
+            //var k= 0;
+            //for(ulong p = 0; p < (ulong)int.MaxValue*3; p++) { k++; }
+            
+            QueryProcessCycleTime(us.Handle, out endCycles);
+            lastGCSSample = switchCounter.NextValue();
+            lastTimeSample = stopwatch.ElapsedMilliseconds;
+
+            mos = new ManagementObjectSearcher("SELECT * FROM Win32_Process where Handle = " + us.Id);
+            foreach (ManagementObject obj in mos.Get()) { usPerf = obj; break; }
+            lastCpuTimeSample = (long)((ulong)usPerf.Properties["KernelModeTime"].Value + (ulong)usPerf.Properties["UserModeTime"].Value) - (long)beginCpuTime;
+            
+            lastCyclesSample = endCycles - beginCycles;
+
+            for (var mi = 0; mi < m; mi++)
+                muts[mi].Dispose();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //GC.TryStartNoGCRegion( 50 * 1024 *1024 );
+
+            var start = DateTime.Now;            
+            ResultsList.Items.Clear();
+            stopwatch.Start();
+
+            var cores = (int)numericUpDown1.Value;
+            
+            var procTime = us.TotalProcessorTime;            
+
+            Action<int> DoTest = delegate (int iters)
+            {
+                SwitchTest( iters, cores);
+
+                //This figure is some how half as much as it should be when we select the first 4 CPUs (on a 5600)
+                var cpuPercent = (lastCpuTimeSample / (long)lastTimeSample)/100.0;
+
+                var globalCSpersec = (long)lastGCSSample;
+                
+                //There were 2 threads runing a loop of 'iter' count, so total calls was 2x
+                iters = iters * 2 * cores;
+
+                var totalGCS = (globalCSpersec * lastTimeSample) /1000;
+
+                var listItem = new ListViewItem( iters/1000 + "k");
+                listItem.SubItems.Add(globalCSpersec.ToString("#,##0"));
+                listItem.SubItems.Add((iters / lastTimeSample).ToString("#,##0"));
+                listItem.SubItems.Add((lastCyclesSample / (ulong)iters).ToString("#,##0"));
+                listItem.SubItems.Add(((double)totalGCS/iters).ToString("#,##0.###"));
+                listItem.SubItems.Add(cpuPercent.ToString("0.###"));
+                ResultsList.Items.Add(listItem);
+                
+            };
+
+            DoTest(50000);
+            DoTest(200000);
+            DoTest(50000);
+            DoTest(500000);
+            DoTest(50000);
+
+            button2.Text = (DateTime.Now - start).ToString();
+
+            switchCounter.Dispose();
+
+            //GC.EndNoGCRegion();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var cpus = Environment.ProcessorCount;
+            var mask = (Int64)Math.Ceiling( Math.Pow(2, cpus)) - 1;
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)mask;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelBackgroundCS.Text = "Background CS/s: " + backgroundCS.NextValue().ToString("#,##0");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)0xF;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var cpus = Environment.ProcessorCount;
+            var bits = Math.Pow(2, cpus - 1) + Math.Pow(2, cpus - 2) + Math.Pow(2, cpus - 3) + Math.Pow(2, cpus - 4);
+            var mask = (Int64)Math.Ceiling( bits);
+            Process.GetCurrentProcess().ProcessorAffinity = (IntPtr)mask;
+        }
+    }
+}
