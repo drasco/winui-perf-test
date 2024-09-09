3fab478385b722ab725d660ae67e6382ec0b2e20
Michael <mlynch@networkanswer.com.au>
2024-09-09 12:37:53 +1000

    Fix WinGDI project name

--- WinGDITesterFramework/Properties/AssemblyInfo.cs
+++ WinGDITesterFramework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinGDSTesterFramework")]
+[assembly: AssemblyTitle("WinGDITesterFramework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinGDSTesterFramework")]
+[assembly: AssemblyProduct("WinGDITesterFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
